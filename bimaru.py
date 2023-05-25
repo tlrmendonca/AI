0d3573958450100579ea34635b93f7dde136b9d7
João Azevedo <joaochartersazevedo@tecnico.ulisboa.pt>
2023-05-25 14:32:38 +0100

    Co-authored-by: tomas <tlrmendonca@users.noreply.github.com>

--- bimaru.py
+++ bimaru.py
@@ -6,7 +6,7 @@
 # 00000 Nome1
 # 00000 Nome2
 
-import sys
+import numpy as np
 from sys import (
     stdin
 )
@@ -22,17 +22,16 @@ from search import (
     recursive_best_first_search,
 )
 
-class TileState(Enum):
-    CENTER = 1
-    UP = 2
-    DOWN = 3
-    LEFT = 4
-    RIGHT = 5
-    MID = 6
-    MID_HORIZONTAL = 7
-    MID_VERTICAL = 8
-    WATER = 9
-    EMPTY = 10
+CENTER = 'C'
+UP = 'T'
+DOWN = 'B'
+LEFT = 'L'
+RIGHT = 'R'
+MID = 'M'
+MID_HORIZONTAL = 'MH'
+MID_VERTICAL = 'MV'
+WATER = 'W'
+EMPTY = 'E'
 
 class Line:
     def __init__(self,total,water,boats):
@@ -49,13 +48,16 @@ class Line:
             return True
         
     def addWater(self):
-        self.water += 1;
+        self.water += 1
 
     def addBoat(self):
-        self.boats += 1;
+        self.boats += 1
 
     def getBoatProbability(self) -> int:
         return (self.total - self.boats) // (10 - self.boats - self.water)
+    
+    def getWaterProbability(self) -> int:
+        return 100 - self.getBoatProbability()
 
 class Action:
     def __init__(self, initial, final):
@@ -85,9 +87,12 @@ class BimaruState:
 class Board:
     """Representação interna de um tabuleiro de Bimaru."""
 
-    board_matrix = list()
-    rows = list()
-    columns = list()
+    board_matrix = list() #board representation
+    rows = list() #list of Line values
+    columns = list() #list of Column values
+
+    placed_boats : int = 0
+    placed_waters : int = 0
 
     def update(self):
         pass
@@ -96,27 +101,32 @@ class Board:
         """Devolve o valor na respetiva posição do tabuleiro."""
         return self.board_matrix[row][col]
     
-    def set_value(self, row: int, col: int, type: TileState):
-        self.board_matrix[row][col] = TileState
+    def set_value(self, row: int, col: int, type):
+        self.board_matrix[row][col] = type
+        
+        if(type == WATER):
+            self.placed_waters += 1
+        else:
+            self.placed_boats += 1
 
     def adjacent_vertical_values(self, row: int, col: int):
-        """Devolve os valores imediatamente acima e abaixo,
-        respectivamente."""
-        values = list()
-        if(row < 10):
-            values.append(self.board_matrix[row+1][col])
-        if(row > 0):
-            values.append(self.board_matrix[row-1][col])
-        return values
-
-    def adjacent_horizontal_values(self, row: int, col: int):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
         values = list()
-        if(col < 10):
+        if(col < 9):
             values.append(self.board_matrix[row][col+1])
         if(col > 0):
             values.append(self.board_matrix[row][col-1])
+        return values
+
+    def adjacent_horizontal_values(self, row: int, col: int):
+        """Devolve os valores imediatamente acima e abaixo,
+        respectivamente."""
+        values = list()
+        if(row < 9):
+            values.append(self.board_matrix[row+1][col])
+        if(row > 0):
+            values.append(self.board_matrix[row-1][col])
         return values
 
     @staticmethod
@@ -130,26 +140,26 @@ class Board:
             > from sys import stdin
             > line = stdin.readline().split()
         """
-
+        
         row = stdin.readline().split()
         column = stdin.readline().split()
         n = stdin.readline().split()
         board = Board()
+
         for i in range(1,11):
-            line = line(int(row[i]),0,0)
+            line = Line(int(row[i]),0,0)
             board.rows.append(line)
 
         for i in range(1,11):
-            line = line(int(column[i]),0,0)
+            line = Line(int(column[i]),0,0)
             board.columns.append(line)
 
         for i in range(0,10):
             row = list()
             for j in range(0,10):
-                row.append(TileState.EMPTY)
+                row.append(EMPTY)
             board.board_matrix.append(row)
             
-        
         for i in range(0,int(n[0])):
             hint = stdin.readline().split()
             x = int(hint[1])
@@ -157,24 +167,26 @@ class Board:
             tile_type = hint[3]
             #W (water), C (circle), T (top), M (middle),B (bottom), L (left) e R (right).
             if tile_type == 'W':
-                board.board_matrix[x][y].addWater += 1;
-                real_type = TileState.WATER
+                board.rows[x].addWater()
+                board.columns[y].addWater()
+                real_type = WATER
             else:
-                board.board_matrix[x][y].addBoat += 1;
+                board.rows[x].addBoat()
+                board.columns[y].addBoat()
                 if tile_type == 'C':
-                    real_type = TileState.CENTER
+                    real_type = CENTER
                 elif tile_type == 'T':
-                    real_type = TileState.UP
+                    real_type = UP
                 elif tile_type == 'M':
-                    real_type = TileState.MID
+                    real_type = MID
                 elif tile_type == 'B':
-                    real_type = TileState.DOWN
+                    real_type = DOWN
                 elif tile_type == 'L':
-                    real_type = TileState.LEFT
+                    real_type = LEFT
                 elif tile_type == 'R':
-                    real_type = TileState.RIGHT
+                    real_type = RIGHT
             
-            board.board_matrix[x][y] = real_type
+            board.set_value(x, y, real_type)
         print("Board created!")
         return board
     
@@ -185,58 +197,14 @@ class Bimaru(Problem):
         """O construtor especifica o estado inicial."""
         self.initial = board
         self.current = board
-
-    def checkWater(self):
-        actionList = list()
-        for i in range(0, 10):
-            modifiedBoard = self.initial
-            if(self.initial.rows[i].checkWater()):
-                for j in range(0,10):
-                    if(self.current.board_matrix[i][j] == TileState.EMPTY):
-                        modifiedBoard.board_matrix[i][j] = TileState.WATER
-                modifiedBoard.update()
-                actionList.append(modifiedBoard)
-            if(self.initial.columns[i].checkWater()):
-                for j in range(0,10):
-                    if(self.current.board_matrix[j][i] == TileState.EMPTY):
-                        modifiedBoard.board_matrix[j][i] = TileState.WATER
-                modifiedBoard.update()
-                actionList.append(modifiedBoard)
-        return actionList
-    
-    def checkFill(self):
-        actionList = list()
-        for i in range(0, 10):
-            modifiedBoard = self.initial
-            if(self.initial.rows[i].checkFill()):
-                for j in range(0,10):
-                    if(self.current.board_matrix[i][j] == TileState.EMPTY):
-                        modifiedBoard.board_matrix[i][j] = TileState.MID
-                modifiedBoard.update()
-                actionList.append(modifiedBoard)
-            if(self.initial.columns[i].checkFill()):
-                for j in range(0,10):
-                    if(self.current.board_matrix[j][i] == TileState.EMPTY):
-                        modifiedBoard.board_matrix[j][i] = TileState.MID
-                modifiedBoard.update()
-                actionList.append(modifiedBoard)
-        return actionList
-
-    def algorithm(self):
-        actionList = list()
-        actionList.append(self.checkWater(self))
-        actionList.append(self.checkFill(self))
         
-
     def pickHighestProbability(self):
-        pass
+        actionList = list()
 
     def actions(self, state: BimaruState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
         actionList = list()
-        actionList.append(self.algorithm())
-        actionList.append(self.pickHighestProbability())
         
         return [action for action in actionList]
     
@@ -245,24 +213,23 @@ class Bimaru(Problem):
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        state.board = action;
+        state.board = action
 
     def goal_test(self, state: BimaruState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas de acordo com as regras do problema."""
-        # TODO
-        pass
+        if(state.board.placed_waters + state.board.placed_boats != 100):
+            return False
+        return True
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
-        # TODO
-        pass
-
-    # TODO: outros metodos da classe
+        return 20 - node.state.board.placed_boats
 
 
 if __name__ == "__main__":
     print("Program Started")
     board = Board.parse_instance()
     bimaru = Bimaru(board)
+    
